b045a119194d0ed17c0d3413738b6adbd792938c
Rajeswari Depala <dsoni071rajeswari@gmail.com>
2024-06-12 10:56:33 -0500

    'updatess'

--- app/page.js
+++ app/page.js
@@ -4,8 +4,22 @@ import CompletePage from '@/components/CompletePage';
 
 export default function Home() {
   return (
-    <div>
-      <CompletePage />
-    </div>
+    <html>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <style>
+          {`
+            body {
+              touch-action: none;
+            }
+          `}
+        </style>
+      </head>
+      <body>
+        <div>
+          <CompletePage />
+        </div>
+      </body>
+    </html>
   );
 }
