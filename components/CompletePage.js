f196beeede0425ef7dd6d7c7c7856fe8feaf6436
Rajeswari Depala <dsoni071rajeswari@gmail.com>
2024-06-13 15:13:42 -0500

    complete css

--- components/CompletePage.js
+++ components/CompletePage.js
@@ -8,7 +8,11 @@ import { useMediaQuery } from 'react-responsive';
 import imageUrl from './myimageraj.jpg';
 import Modal from './Modal'; // Import the modal component
 
+import { SiPytorch, SiAnaconda, SiPython, SiSpringboot, SiReact, SiExpress, SiC, SiLinux, SiOracle, SiR } from 'react-icons/si';
+import { FaDog } from 'react-icons/fa'; // Using FontAwesome for API Dog
+
 import { hsl, parseToHsl } from 'polished';
+import zIndex from '@mui/material/styles/zIndex';
 
 const LoaderWrapper = styled('div')({
   position: 'fixed',
@@ -159,12 +163,12 @@ const borderRing = (colors, border, gap) => {
 };
 
 const StyledImage = styled('img')`
-  width: 60%;
+width: 150px; // Adjust width as needed
+  height: 430px; // Adjust height as needed
   border-radius: 50%;
   display: block;
-  height: 80%;
   font-family: 'Montserrat, sans-serif';
-  filter: hue-rotate(15deg);
+  filter: hue-rotate(17deg);
   transition: filter 0.3s ease;
 
   &:hover {
@@ -185,16 +189,19 @@ const SummaryText = styled('p')(({ isMobile }) => ({
 
 const Section = styled('div')({
   position: 'relative',
-  marginLeft: '120px',
+  marginLeft: '20px',
   textAlign: 'left',
-
+  display: 'flex',
+  alignItems: 'center', // Align items vertically in the center
   '& p': {
+    display: 'inline-block',
     position: 'relative',
     paddingLeft: '1.2em',  // Adjust this value as needed for spacing
     fontSize: '1.95rem',
     fontWeight: 'bold',
     color: '#274c77',
     lineHeight: '1.95rem',  // Ensure the line height matches the font size
+    margin: '0',  // Remove default margin
     
     '&::before': {
       content: '"<"',  // Use a checkmark instead of a bullet point
@@ -214,15 +221,12 @@ const Section = styled('div')({
       marginLeft: '0.2em',  // Add a little space between text and closing symbol
     }
   },
-});
-
-const EducationGrid = styled('div')({
-  margin: '0 10%',
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '10px',
-  alignItems: 'center',
-  fontFamily: 'Montserrat, sans-serif',
+  '& svg': {
+    marginLeft: '20px', // Add some space between the text and the line
+    flexShrink: 0, // Prevent the line from shrinking
+    width: '100%',
+    opacity : '2',
+  }
 });
 
 const StyledH1 = styled('h1')(({ isMobile }) => ({
@@ -248,11 +252,20 @@ const JobTitle = styled('p')(({ isMobile }) => ({
   fontFamily: 'Montserrat, sans-serif',
 }));
 
+const EducationGrid = styled('div')({
+  margin: '5%',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '10px',
+  alignItems: 'center',
+  fontFamily: 'Montserrat, sans-serif',
+});
+
 const EducationCard = styled('div')({
   backgroundColor: '#d5deed',
   borderRadius: '12px',
   boxShadow: '0 4px 12px rgba(0, 0, 0, 0.3)',
-  padding: '10px',
+  padding: '20px',
   color: '#000000',
   opacity: '0.75',
   transition: 'transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out',
@@ -291,7 +304,9 @@ const EducationCard = styled('div')({
     fontSize: '1.75rem',
     fontWeight: 'bold',
     margin: '15px 0',
-    color: '#2c3e50',
+    opacity: 9,
+    zIndex:9,
+    color: '#274c77',
   },
 
   '& p': {
@@ -308,6 +323,21 @@ const EducationCard = styled('div')({
   borderRadius: '12px',
   mixBlendMode: 'multiply', 
   backgroundColor: 'transparent', 
+},
+'& .inst': {
+  fontSize: '1.2rem',
+  fontWeight: '600',
+  color: '#274c77',
+  margin: '10px 0',
+  textAlign: 'center',
+},
+
+'& .des': {
+  fontSize: '1rem',
+  fontStyle: 'italic',
+  color: '#555555',
+  margin: '10px 0',
+  textAlign: 'justify',
 },
 });
 
@@ -528,6 +558,7 @@ const Timeline = styled('div')({
   },
 });
 
+
 const ProjectsTab = styled('div')`
   html, body {
     height: 100%;
@@ -600,7 +631,7 @@ const ProjectsTab = styled('div')`
   .content {
     display: none;
     padding: 20px;
-    background-color: #f5f5f5;
+    background-color: none;
     border-radius: 8px;
     color: #333;
     margin-bottom: 20px;
@@ -652,6 +683,120 @@ const ProjectsTab = styled('div')`
     border-radius: 0 32px 0 0;
   }
 `;
+
+const TechLogo = styled('img')`
+  width: 60px;
+  height: 60px;
+  margin: 10px 20px 10px 10px ;
+`;
+
+const ProjectContent = styled('div')`
+  .tech-icons {
+    display: flex;
+    align-items: center;
+    margin-top: 10px;
+  }
+
+  .carousel {
+    width: 80%;
+    overflow: hidden;
+  }
+
+  .carousel__track {
+    display: flex;
+    width: calc(82px * 12); /* Adjust based on the number of images */
+    animation: scroll 20s linear infinite;
+  }
+
+  .carousel__images {
+    display: flex;
+    width: calc(80px * 8); /* Adjust based on the number of images */
+  }
+
+  .carousel__images img {
+    width: 120px;
+    height: 150px;
+    object-fit: cover;
+    margin: 15px 15px 15px 15px;
+    border-radius: 8px;
+    transition: transform 0.3s ease-in-out;
+  }
+
+  .carousel__images img:hover {
+    transform: scale(1.1);
+    cursor: pointer;
+  }
+
+  .carousel__images:hover .carousel__track {
+    animation-play-state: paused;
+  }
+
+  .overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.8);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out, visibility 0.3s ease-in-out;
+  }
+
+  .overlay img {
+    max-width: 80%;
+    max-height: 80%;
+    border-radius: 8px;
+  }
+
+  .overlay.active {
+    visibility: visible;
+    opacity: 1;
+  }
+
+  .projex {
+    flex-basis: 100%;
+    color: black;
+    overflow: hidden;
+    position: relative;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    text-overflow: ellipsis;
+    transition: max-height 0.3s ease;
+    margin: 10px 0px;
+  }
+
+  .projex.expanded {
+    webkit-line-clamp: unset;
+    max-height: none;
+  }
+  .show-more {
+    display: block;
+    background-color: transparent;
+    border: none;
+    color: #7f8c8d;
+    cursor: pointer;
+    text-align: left;
+    padding: 0;
+    margin-top: 5px;
+    font-family: Montserrat, sans-serif;
+  }
+
+  @keyframes scroll {
+    0% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(calc(-80px * 8)); /* Adjust based on the number of images */
+    }
+  }
+`;
+
 
 const Certificates = styled('div')({
   display: 'grid',
@@ -726,7 +871,6 @@ const FooterContainer = styled('footer')(({ isMobile }) => ({
   overflow: 'hidden',
 }));
 
-
 const SocialIconList = styled('ul')({
   display: 'flex',
   justifyContent: 'center',
@@ -774,6 +918,7 @@ const CompletePage = () => {
   const experienceRef = useRef(null);
   const projectsRef = useRef(null);
   const certificationRef = useRef(null);
+  const [overlayImage, setOverlayImage] = useState(null);
 
   const handleTabChange = (index) => {
     setActiveTab(index);
@@ -856,6 +1001,50 @@ const CompletePage = () => {
     setSelectedImage(image);
     setShowModal(true);
   };
+  const getTechLogo = (tech) => {
+    switch (tech.toLowerCase()) {
+      case 'pytorch':
+        return 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytorch/pytorch-original.svg';
+      case 'anaconda':
+        return 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/anaconda/anaconda-original.svg';
+      case 'python':
+        return 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg';
+      case 'java springboot':
+        return 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg';
+      case 'reactjs':
+        return 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg';
+      case 'express.js':
+        return 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg';
+      case 'c':
+        return 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg';
+      case 'linux':
+        return 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg';
+      case 'oracle vm virtualbox':
+        return 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Virtualbox_logo.png'; // Custom URL for Oracle VM VirtualBox
+      case 'r':
+        return 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/r/r-original.svg';
+      case 'api dog':
+        return 'https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg'; 
+      default:
+        return null;
+    }
+  };
+
+  const renderCarousel = (projectTitle, imageCount = 8) => {
+    const images = [];
+    for (let i = 1; i <= imageCount; i++) {
+      images.push(
+        <img key={`${i}-original`} src={`/projects/${projectTitle}/${i}.jpg`} alt={`${projectTitle} ${i}`} />
+      );
+    }
+    // Duplicate the images for a seamless loop
+    for (let i = 1; i <= imageCount; i++) {
+      images.push(
+        <img key={`${i}-duplicate`} src={`/projects/${projectTitle}/${i}.jpg`} alt={`${projectTitle} ${i}`} />
+      );
+    }
+    return <div className="carousel__images">{images}</div>;
+};
 
   return (
     <>
@@ -913,7 +1102,11 @@ const CompletePage = () => {
               onError={(e) => console.error('Error loading image:', e.target.src, e)}
             />
           </UserGrid>
-          <Section ref={educationRef}><p >Academics</p></Section>
+          <Section ref={educationRef}><p >Knowledge from</p>
+          <svg height="2" width="66%">
+      <line x1="0" y1="1" x2="66%" y2="1" style={{ stroke: '#274c77', strokeWidth: 1 }} />
+    </svg>
+          </Section>
           <EducationGrid>
             <EducationCard>
               <h3>
@@ -928,8 +1121,8 @@ const CompletePage = () => {
                   }
                 }}
               />
-              <p>{userData.educations[activeIndex].institution}</p>
-              <p>{userData.educations[activeIndex].description}</p>
+              <p className='inst'>{userData.educations[activeIndex].institution}</p>
+              <p className='des'>{userData.educations[activeIndex].description}</p>
             </EducationCard>
             <Dots>
               {userData.educations.map((_, index) => (
@@ -937,7 +1130,10 @@ const CompletePage = () => {
               ))}
             </Dots>
           </EducationGrid>
-          <Section ref={experienceRef}> <p>Where I&apos;ve worked</p></Section>
+          <Section ref={experienceRef}> <p>Where I&apos;ve worked</p>
+          <svg height="2" width="66%">
+      <line x1="0" y1="1" x2="66%" y2="1" style={{ stroke: '#274c77', strokeWidth: 1 }} />
+    </svg></Section>
           <Timeline>
             {userData.experiences.map((experience, index) => (
               <div key={index} className="timeline__event animated fadeInUp delay-3s timeline__event--type1">
@@ -961,7 +1157,9 @@ const CompletePage = () => {
               </div>
             ))}
           </Timeline>
-          <Section ref={projectsRef}><p>Skills I&apos;ve applied</p></Section>
+          <Section ref={projectsRef}><p>Skills I&apos;ve applied</p> <svg height="2" width="66%">
+      <line x1="0" y1="1" x2="66%" y2="1" style={{ stroke: '#274c77', strokeWidth: 1 }} />
+    </svg></Section>
           <ProjectsTab>
             <div className="popup">
               <div className="tabs">
@@ -978,18 +1176,37 @@ const CompletePage = () => {
               </div>
               <div className="content-wrapper">
                 {userData.projects.map((project, index) => (
-                  <div key={index} className={`content ${activeTab === index ? 'active' : ''}`}>
-                    <p>{project.description}</p>
-                    <p><strong>Technologies Used:</strong> {project.technologies.join(', ')}</p>
-                    <a href={project.glink} target="_blank" rel="noopener noreferrer">
-                      View Project
-                    </a>
+                  <ProjectContent key={index} className={`content ${activeTab === index ? 'active' : ''}`}>
+                  <div className="carousel">
+                    <div className="carousel__track">{renderCarousel(project.title)}</div>
                   </div>
+                  {overlayImage && (
+                  <div className="overlay active" onClick={() => setOverlayImage(null)}>
+                    <img src={overlayImage} alt="Overlay" />
+                   </div>
+                  )}
+                  <div className="tech-icons">
+                    {project.technologies.map((tech, techIndex) => (
+                      <TechLogo key={techIndex} src={getTechLogo(tech)} alt={tech} />
+                    ))}
+                  </div>
+                  <div className={`projex ${showFullDescription[index] ? 'expanded' : ''}`}>
+                    <div dangerouslySetInnerHTML={{ __html: project.description }} />
+                  </div>
+                  <button className="show-more" onClick={() => toggleDescription(index)}>
+                    {showFullDescription[index] ? 'Show Less' : 'Show More'}
+                  </button>
+                  <a href={project.glink} target="_blank" rel="noopener noreferrer">
+                    View Project
+                  </a>
+                </ProjectContent>
                 ))}
               </div>
             </div>
           </ProjectsTab>
-          <Section ref={certificationRef}><p >Credentials Acquired</p></Section>
+          <Section ref={certificationRef}><p >Credentials earned</p> <svg height="2" width="66%">
+      <line x1="0" y1="1" x2="66%" y2="1" style={{ stroke: '#274c77', strokeWidth: 1 }} />
+    </svg></Section>
           <Certificates>
             {userData.certifications.map((certification, index) => (
               <List key={index}>
