222a510ce2e2c6f12d3a6ddecac9cec0bbad5e4f
Rajeswari Depala <dsoni071rajeswari@gmail.com>
2024-06-12 22:15:38 -0500

    another final css upates

--- components/CompletePage.js
+++ components/CompletePage.js
@@ -788,7 +788,7 @@ const CompletePage = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:3001//api/users?firstName=Depala&lastName=Rajeswari');
+        const response = await axios.get('https://rajeswari-depala.netlify.app/api/users?firstName=Depala&lastName=Rajeswari');
         setUserData(response.data);
         console.log('Image URL:', imageUrl);
         setLoading(false);
